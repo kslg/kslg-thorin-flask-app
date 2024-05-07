aa41932e55b1e0d8a1bbc819a498bbb622838c19
Krishan <kslg257@gmail.com>
2024-05-07 09:43:45 +0000

    Updated the flash message.

--- run.py
+++ run.py
@@ -55,7 +55,7 @@ def about_member(member_name):
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
     if request.method == "POST":
-        flash("Thanks {}, we have received your message!".format(
+        flash("Thank you {}, we have received your message!".format(
             request.form.get("name")))
     return render_template("contact.html", page_title="Contact")
 
